f07586496ff280244d595cba6d043abc6424a1fe
Sakshi Gupta <40806036+sakshigupta1997@users.noreply.github.com>
2018-09-20 00:08:43 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,7 @@
 public class sum{
     public static void main(String args[]){
-int a,b;
-        System.out.println(a+b);
+int a=4,b=3,sum;
+        sum=a+b;
+        System.out.println(sum);
     }
 }
